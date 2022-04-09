339f27b99eca95c16855478b4192e16221b6857a
KostyaSamyshkin <101719592+KostyaSamyshkin@users.noreply.github.com>
2022-04-09 16:44:15 +0700

    Добавил проверку на неправильный пароль

--- AESDemonstration/MainForm.cs
+++ AESDemonstration/MainForm.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Text;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 
 namespace AESDemonstration
@@ -15,15 +15,22 @@ namespace AESDemonstration
         private void btnEncrypt_Click(object sender, EventArgs e) // Зашифровать текст
         {
             txtCryptoText.Text = AESUtils.Encrypt(txtRawText.Text, txtPassword.Text);
-            
+
             txtRawText.Text = "";
         }
 
         private void btnDecrypt_Click(object sender, EventArgs e) // Расшифровать текст
         {
-            txtRawText.Text = AESUtils.Decrypt(txtCryptoText.Text, txtPassword.Text);
+            try
+            {
+                txtRawText.Text = AESUtils.Decrypt(txtCryptoText.Text, txtPassword.Text);
             
-            txtCryptoText.Text = "";
+                txtCryptoText.Text = "";
+            }
+            catch (CryptographicException)
+            {
+                MessageBox.Show(this, "Неверный пароль!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
